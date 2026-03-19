77696f49d41ad405af0d773115724806ab2875b5
Kunwar Grover <groverkss@gmail.com>
2026-03-19 12:44:06 +0000

    more legalizations and move some patterns to 1d canonicalizations

--- compiler/src/iree/compiler/Codegen/LLVMGPU/Passes.cpp
+++ compiler/src/iree/compiler/Codegen/LLVMGPU/Passes.cpp
@@ -996,13 +996,16 @@ static void addLowerToLLVMGPUPasses(OpPassManager &modulePassManager,
       .addPass(createCanonicalizerPass)
       .addPass(createCSEPass);
 
-  // This pass needs to run before SCF -> CF.
+  // Lower vector operations and legalize all operations to 1D vectors.
   funcPassManager.addPass(createLLVMGPUVectorLoweringPass)
       .addPass(createLLVMGPULegalizeNDVectorsPass)
       .addPass(createCanonicalizerPass)
       .addPass(createCSEPass);
 
   addLowerAndOptimizeAddressComputationPasses(funcPassManager);
+
+  // Canonicalize with a restriction that all vector operations are 1D.
+  funcPassManager.addPass(createLLVMGPU1DVectorCanonicalizationsPass);
 
   if (forROCDL) {
     // This pass needs to run after the LLVMGPUVectorLoweringPass.
